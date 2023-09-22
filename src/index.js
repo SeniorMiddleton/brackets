a95a0bc61863683d6a862710a5746878d321734c
Senior_Middleton <126986498+SeniorMiddleton@users.noreply.github.com>
2023-09-22 15:44:48 +0300

    init:Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+ const stack = [];
+  const openingBrackets = new Set();
+  const closingBrackets = new Set();
+  const matchingBrackets = {};
+
+  for (const [open, close] of bracketsConfig) {
+    openingBrackets.add(open);
+    closingBrackets.add(close);
+    matchingBrackets[close] = open;
+  }
+
+  for (const char of str) {
+    if (openingBrackets.has(char)) {
+      if (
+        stack.length > 0 &&
+        stack[stack.length - 1] === char &&
+        closingBrackets.has(char)
+      ) {
+       
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else if (closingBrackets.has(char)) {
+      if (stack.length === 0 || stack.pop() !== matchingBrackets[char]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
